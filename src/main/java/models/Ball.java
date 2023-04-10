4868c5990d5b4236bda7ba52cefdbe28fd56743a
arfernandez2000 <arfernandez@itba.edu.ar>
2023-04-10 11:20:59 -0300

    balls

---
+++ src/main/java/models/Ball.java
@@ -0,0 +1,58 @@
+package models;
+
+public class Ball {
+    private int x;
+    private int y;
+    private int radius;
+    private int speedX;
+    private int speedY;
+
+    public Ball(int x, int y, int radius, int speedX, int speedY) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.speedX = speedX;
+        this.speedY = speedY;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+
+    public int getSpeedX() {
+        return speedX;
+    }
+
+    public void setSpeedX(int speedX) {
+        this.speedX = speedX;
+    }
+
+    public int getSpeedY() {
+        return speedY;
+    }
+
+    public void setSpeedY(int speedY) {
+        this.speedY = speedY;
+    }
+
+}
